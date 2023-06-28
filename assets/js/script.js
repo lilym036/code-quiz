7a30ff6ff0a37fa3a3faaa8dcea819a6dc1e2249
Lilibeth Mejia <lilibethmejia@Lilibeths-Air.attlocal.net>
2023-06-28 17:21:37 -0500

    added to the README and worked on figuring out local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@ var timeEl = document.querySelector(".time");
 var secondsLeft = 75;
 var startButton = document.querySelector(".start-button");
 var choices = document.querySelector(".choices");
-var startScreen= document.querySelector(".start-screen");
-var quizStart= document.querySelector(".quiz-start");
-var qTitle= document.querySelector(".qTitle");
-var currentQuestion= 0;
+var startScreen = document.querySelector(".start-screen");
+var quizStart = document.querySelector(".quiz-start");
+var qTitle = document.querySelector(".qTitle");
+var formStart = document.querySelector(".form-start");
+var currentQuestion = 0;
+var finalScore = document.querySelector(".final-score");
+var submitForm = document.querySelector(".submit-form");
+var initialsEl = document.querySelector("#initials-text");
+var submitButton = document.querySelector(".submit-btn");
 var timerInterval;
 var questions = [{
     questionTitle: "School Psychologists work in:",
@@ -17,7 +22,7 @@ var questions = [{
     correctAnswer: "3.Autism Spectrum Disorder",
 }, {
     questionTitle: "The following are presentations of ADHD except:",
-    questionChoices: ["1.Predominately Inattentive Type", "2.Predominately Hyperactive/Impulsive Type", "3.Predominately Very Excitable Type", "4.Combined Type 4"],
+    questionChoices: ["1.Predominately Inattentive Type", "2.Predominately Hyperactive/Impulsive Type", "3.Predominately Very Excitable Type", "4.Combined Type"],
     correctAnswer: "3.Predominately Very Excitable Type",
 }, {
     questionTitle: "In order to qualify under the Other Health Impairment (OHI) eligbility a student must demonstrate limitations in the following areas except:",
@@ -29,6 +34,7 @@ var questions = [{
     correctAnswer: "1.Individualized Education Program",
 }];
 console.log(questions);
+
 
 function setTime() {
     timerInterval = setInterval(function () {
@@ -46,7 +52,7 @@ function startGame() {
     setTime();
     startScreen.classList.add("hide");
     quizStart.classList.remove("hide");
-    showQuestions()
+    showQuestions();
 }
 
 function checkAnswer() {
@@ -54,10 +60,11 @@ function checkAnswer() {
     if (this.dataset.value === questions[currentQuestion].correctAnswer) {
         currentQuestion++;
     } else {
-        secondsLeft= secondsLeft - 10;
+        secondsLeft = secondsLeft - 10;
     }
-    if (secondsLeft <= 0 ||currentQuestion=== questions.length) {
-        clearInterval(timerInterval);
+    if (secondsLeft <= 0 || currentQuestion === questions.length) {
+        clearInterval(timerInterval)
+        form();
     } else {
         showQuestions()
     }
@@ -65,17 +72,41 @@ function checkAnswer() {
 
 
 function showQuestions() {
-    qTitle.textContent= questions[currentQuestion].questionTitle;
-    choices.textContent= "";
-    for (let i= 0; i < questions[currentQuestion].questionChoices.length; i++) {
-        var button= document.createElement("button");
-        button.textContent= questions[currentQuestion].questionChoices[i];
+    qTitle.textContent = questions[currentQuestion].questionTitle;
+    choices.textContent = "";
+    for (let i = 0; i < questions[currentQuestion].questionChoices.length; i++) {
+        var button = document.createElement("button");
+        button.textContent = questions[currentQuestion].questionChoices[i];
         button.setAttribute("data-value", questions[currentQuestion].questionChoices[i])
         button.addEventListener("click", checkAnswer)
         choices.appendChild(button);
     }
 }
 
+function form() {
+    quizStart.classList.add("hide");
+    formStart.classList.remove("hide");
+    finalScore.textContent = "Your final score is " + secondsLeft;
+}
 
-startButton.addEventListener ("click", startGame);
+// submitButton.addEventListener("sumbit", function (event) {
+//     event.preventDefault();
+//     var savedScores = {
+//         student: initialsEl.value,
+//         highScore: finalScore.value,
+//     };
+//     localStorage.setItem("savedScores", JSON.stringify(savedScores));
+//     renderMessage();
+// });
+
+// function renderMessage() {
+//     var lastScore = JSON.parse(localStorage.getItem("savedScores"));
+//     if (lastScore !== null) {
+//       document.querySelector(".message").textContent =  lastScore.initialsEl + 
+//       " received a score of" + lastScore
+//     }
+//   }
+  
+
+startButton.addEventListener("click", startGame);
 
